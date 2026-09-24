6bb9a7f0a0f2884899b295d39db6c720d47adc26
luckyli1983 <luckyli1983@qq.com>
2026-09-24 13:40:41 +0800

    Update Test.cs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TEsst ver1.1");
+        Debug.Log("TEsst ver1.2");
     }
 
     // Update is called once per frame
